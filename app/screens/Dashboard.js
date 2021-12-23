b3c4b013aea0b0af2d0fe819fe5de9b2d7e6322d
bakano98 <70756909+bakano98@users.noreply.github.com>
2021-12-24 00:58:02 +0800

    remove unused

--- app/screens/Dashboard.js
+++ app/screens/Dashboard.js
@@ -11,21 +11,22 @@ import {
   View,
   Image,
 } from "react-native";
-import * as dateFn from 'date-fns';
+import * as dateFn from "date-fns";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { getModeMood,
-        getModeMoodArray, 
-        getProgress, 
-        getTrend,
-        toDict,
-        displayModeMood, } from './Stats/DataProcessing';
+import {
+  getModeMood,
+  getModeMoodArray,
+  getProgress,
+  getTrend,
+  toDict,
+  displayModeMood,
+} from "./Stats/DataProcessing";
 import PieChartWeek from "./Stats/PieChartWeek";
 import PieChartMonth from "./Stats/PieChartMonth";
 import ProgressBar from "./Stats/ProgessBar";
 import StackedGraph from "./Stats/StackedGraph";
 import LineGraph from "./Stats/LineGraph";
 import PieChartYear from "./Stats/PieChartYear";
-
 
 // image is just a placeholder for now
 const icons = require("../icons/icons.js");
@@ -45,83 +46,77 @@ const Dashboard = ({ navigation }) => {
   // Note that if we want to update anything related to the state, we have to directly call user_state.(dataType) = ....
   const moodState = useSelector((state) => state);
   const moodsData = moodState.data;
-  console.log(moodsData)
-  const logPoints = moodState.logPoints;  
-  
+  // console.log(moodsData)
+  const logPoints = moodState.logPoints;
 
   //parse data into a dictionary
   const dict = toDict(moodsData);
   const date = new Date();
-  const months = ["January", "February", "March", "April", 
-  "May", "June", "July", "August", "September", "October", 
-  "November", "December"];
-  
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
   //handling empty {} after AsyncStorage.clear()
 
   return (
-      
-    <ImageBackground source={icons["BG_pic"]} style={styles.image}>  
-        
-        
-        
-        
-        <ScrollView
-          contentContainerStyle={{ justifyContent: "center" }}> 
-        
-          <View style = {styles.subcontainer}>
-            <Text style={styles.text}>
-              You feel {getModeMood(moodsData, 5)}.
-            </Text>
-            <View style = {styles.moodContainer}>
-               {displayModeMood(getModeMoodArray(moodsData, 5)) }
-            </View>
+    <ImageBackground source={icons["BG_pic"]} style={styles.image}>
+      <ScrollView contentContainerStyle={{ justifyContent: "center" }}>
+        <View style={styles.subcontainer}>
+          <Text style={styles.text}>You feel {getModeMood(moodsData, 5)}.</Text>
+          <View style={styles.moodContainer}>
+            {displayModeMood(getModeMoodArray(moodsData, 5))}
           </View>
+        </View>
 
-          <View style = {styles.pieContainer}>
-              <Text style = {styles.subheader}> 
-                Summary 
-              </Text>
-              <View style = {{justifyContent: 'center', alignItems:'center'}}>
-                {ProgressBar(dict)}
-                <Text style = {styles.pieText}>{Math.ceil(getProgress(dict) * 100)}%</Text>
-              </View>
-              <View style = {{justifyContent: 'center', alignItems:'center'}}>
-                  {PieChartWeek(dict)}
-                  <Text style = {styles.pieText}>Week {dateFn.getWeek(date)}</Text>
-              </View>
-              <View style = {{justifyContent: 'center', alignItems:'center'}}>
-                  {PieChartMonth(dict)}
-                  <Text style = {styles.pieText}>{months[dateFn.getMonth(date)]}</Text>
-              </View>
-              <View style = {{justifyContent: 'center', alignItems:'center'}}>
-                {PieChartYear(dict)}
-                <Text style = {styles.pieText}>{dateFn.getYear(date)}</Text>
-              </View> 
-          </View>
-          
-          <View style = {styles.graphContainer}>
-            <Text style = {styles.subheader}> 
-              Trend {getTrend(moodsData, 7)}
-            </Text>
-             {LineGraph(moodsData, 7)}
-          </View>
-          
-          <View style={styles.circle}>
-            <Text style={styles.pointsText}>
-              {logPoints}
+        <View style={styles.pieContainer}>
+          <Text style={styles.subheader}>Summary</Text>
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            {ProgressBar(dict)}
+            <Text style={styles.pieText}>
+              {Math.ceil(getProgress(dict) * 100)}%
             </Text>
           </View>
-          
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            {PieChartWeek(dict)}
+            <Text style={styles.pieText}>Week {dateFn.getWeek(date)}</Text>
+          </View>
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            {PieChartMonth(dict)}
+            <Text style={styles.pieText}>{months[dateFn.getMonth(date)]}</Text>
+          </View>
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            {PieChartYear(dict)}
+            <Text style={styles.pieText}>{dateFn.getYear(date)}</Text>
+          </View>
+        </View>
 
-          <Button
-            title="Show data on console"
-            onPress={() => console.log(dict)}
-          />
-          <Button title="Clear whole AsyncStorage" onPress={() => clearAll()} />
-          
-          </ScrollView>
+        <View style={styles.graphContainer}>
+          <Text style={styles.subheader}>Trend {getTrend(moodsData, 7)}</Text>
+          {LineGraph(moodsData, 7)}
+        </View>
+
+        <View style={styles.circle}>
+          <Text style={styles.pointsText}>{logPoints}</Text>
+        </View>
+
+        <Button
+          title="Show data on console"
+          onPress={() => console.log(dict)}
+        />
+        <Button title="Clear whole AsyncStorage" onPress={() => clearAll()} />
+      </ScrollView>
     </ImageBackground>
-    
   );
 };
 
@@ -134,35 +129,33 @@ const styles = StyleSheet.create({
   },
   subcontainer: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: "#f5f5f5",
     borderRadius: 25,
     marginStart: 10,
     marginEnd: 10,
     height: 150,
-    flexDirection: 'column',
+    flexDirection: "column",
     marginTop: 50,
     marginBottom: 10,
-    
   },
   subheader: {
     fontSize: 20,
     color: "black",
-    width:400,
+    width: 400,
     marginStart: 80,
     marginEnd: 0,
-    paddingBottom:10
+    paddingBottom: 10,
   },
   text: {
     fontSize: 20,
     color: "black",
-    height:60,
+    height: 60,
     //backgroundColor: '#f5f5f5',
     //borderRadius: 25,
     //padding: 10,
     marginStart: 25,
     marginEnd: 10,
     paddingTop: 10,
-    
   },
 
   button: {
@@ -178,57 +171,57 @@ const styles = StyleSheet.create({
   },
   moodContainer: {
     paddingHorizontal: 10,
-    flexDirection:'row',
-    justifyContent:'space-around',
+    flexDirection: "row",
+    justifyContent: "space-around",
     paddingTop: 20,
   },
   pieContainer: {
-    backgroundColor: '#f5f5f5',
+    backgroundColor: "#f5f5f5",
     borderRadius: 25,
     padding: 10,
-    justifyContent: 'space-around',
-    flexDirection:'row',
-    flexWrap:'wrap',
+    justifyContent: "space-around",
+    flexDirection: "row",
+    flexWrap: "wrap",
     marginStart: 10,
     marginEnd: 10,
   },
   pieText: {
-    fontStyle: 'italic',
+    fontStyle: "italic",
     fontSize: 18,
     padding: 10,
-    paddingBottom:10,
+    paddingBottom: 10,
   },
   graphContainer: {
-    backgroundColor: '#f5f5f5',
+    backgroundColor: "#f5f5f5",
     borderRadius: 25,
     padding: 10,
-    justifyContent: 'space-around',
-    flexDirection:'row',
-    flexWrap:'wrap',
+    justifyContent: "space-around",
+    flexDirection: "row",
+    flexWrap: "wrap",
     marginStart: 10,
     marginEnd: 10,
     marginTop: 10,
-    marginBottom:10,
+    marginBottom: 10,
   },
   circle: {
-    height:100, 
-    width:100, 
-    backgroundColor: '#f5f5f5', 
-    borderRadius: 100, 
-    overflow: 'hidden', 
+    height: 100,
+    width: 100,
+    backgroundColor: "#f5f5f5",
+    borderRadius: 100,
+    overflow: "hidden",
     opacity: 1,
-    justifyContent: 'center',
-    alignContent:'center',
+    justifyContent: "center",
+    alignContent: "center",
     marginStart: 150,
     marginTop: 10,
-    marginBottom: 10
+    marginBottom: 10,
   },
   pointsText: {
-    color:'black',
+    color: "black",
     fontSize: 40,
     padding: 0,
-    paddingStart: 25
-  }
+    paddingStart: 25,
+  },
 });
 
 export default Dashboard;
