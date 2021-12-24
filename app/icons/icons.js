8b9b0dd87147921d255c3427b9873bb688f5fbf3
bakano98 <70756909+bakano98@users.noreply.github.com>
2021-12-24 11:48:49 +0800

    Update icons.js

--- app/icons/icons.js
+++ app/icons/icons.js
@@ -1,10 +1,6 @@
 // all icons here
 
 const icons = {
-  placeholder: require("./placeholder.jpg"),
-  pic1: require("./pic1.png"),
-  pic2: require("./pic2.png"),
-  pic3: require("./pic3.png"),
   // background pic
   BG_pic: require("./BG2.png"),
   loading_pic: require("./loading_screen.png"),
@@ -17,25 +13,17 @@ const icons = {
   mood_calm: require("./Moods/mood_calm.png"),
   mood_anxious: require("./Moods/mood_anxious.png"),
   mood_angry: require("./Moods/mood_angry.png"),
-  // current tabs
-  settings_s: require("./settings_s.png"),
-  services_s: require("./services_s.png"),
-  resources_s: require("./resources_s.png"),
-  moodscreen_s: require("./moodscreen_s.png"),
-  dashboard_s: require("./dashboard_s.png"),
-  // not current tabs
-  settings_u: require("./settings_u.png"),
-  services_u: require("./services_u.png"),
-  resources_u: require("./resources_u.png"),
-  moodscreen_u: require("./moodscreen_u.png"),
-  dashboard_u: require("./dashboard_u.png"),
+  // tabs
+  settings: require("./settings_u.png"),
+  services: require("./services_u.png"),
+  resources: require("./resources_u.png"),
+  moodscreen: require("./moodscreen_u.png"),
+  dashboard: require("./dashboard_u.png"),
   // floating action button
   float_button: require("./float_button.png"),
   // back-forth arrows for calendar
   arrow_L: require("./arrow_L.png"),
   arrow_R: require("./arrow_R.png"),
-  // lock
-  lock: require("./lock.png"),
 
   // icons for other stuff
   icon_emergency: require("./icon_emergency.png"),
